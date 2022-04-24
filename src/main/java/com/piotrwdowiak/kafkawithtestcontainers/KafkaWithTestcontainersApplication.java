22975146102ef371d65c930913997ba17424dc71
Piotr <piotr@gmail.com>
2022-04-24 13:10:58 +0200

    initial commit

---
+++ src/main/java/com/piotrwdowiak/kafkawithtestcontainers/KafkaWithTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.piotrwdowiak.kafkawithtestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWithTestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWithTestcontainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piotrwdowiak/kafkawithtestcontainers/KafkaWithTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrwdowiak.kafkawithtestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWithTestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
